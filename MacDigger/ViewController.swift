cebc5a44696141e1d5051d0ae6bcb090c1adfcfc
Stas <staspanasuk@mail.ru>
2018-03-22 00:29:54 +0200

    Initial Commit

---
+++ MacDigger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MacDigger
+//
+//  Created by Stas Panasuk on 3/22/18.
+//  Copyright © 2018 iSan4eZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
