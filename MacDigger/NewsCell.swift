a3e4f7c27fc2f34ecaa2077a8ffaab63afcb6003
Stas <staspanasuk@mail.ru>
2018-03-23 17:16:13 +0200

    Main menu TableView prepared

---
+++ MacDigger/NewsCell.swift
@@ -0,0 +1,95 @@
+//
+//  NewsCell.swift
+//  MacDigger
+//
+//  Created by Stas Panasuk on 3/22/18.
+//  Copyright © 2018 iSan4eZ. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class NewsCell : UITableViewCell {
+    
+    var topic : String?
+    var date : String?
+    var mainImage : UIImage?
+    
+    var imagePreview : UIImageView = {
+        var imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    var topicView : UITextView = {
+        var textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.isScrollEnabled = false
+        return textView
+    }()
+    
+    var dateView : UILabel = {
+        var label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = UIColor.gray
+        label.font = label.font.withSize(8)
+        return label
+    }()
+    
+    var rightArrow : UILabel = {
+        var label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = UIColor.white
+        label.text = ">"
+        label.textColor = UIColor.gray
+        return label
+    }()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.addSubview(imagePreview)
+        self.addSubview(topicView)
+        self.addSubview(dateView)
+        self.addSubview(rightArrow)
+        
+        rightArrow.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -5).isActive = true
+        rightArrow.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        rightArrow.textAlignment = .right
+        
+        imagePreview.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 5).isActive = true
+        imagePreview.widthAnchor.constraint(equalToConstant: 75).isActive = true
+        imagePreview.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        imagePreview.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        imagePreview.bottomAnchor.constraint(lessThanOrEqualTo: self.bottomAnchor, constant: -10).isActive = true
+        
+        topicView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        topicView.leftAnchor.constraint(equalTo: imagePreview.rightAnchor, constant: 5).isActive = true
+        topicView.rightAnchor.constraint(lessThanOrEqualTo: rightArrow.leftAnchor, constant: -5).isActive = true
+        
+        dateView.topAnchor.constraint(equalTo: topicView.bottomAnchor).isActive = true
+        dateView.leftAnchor.constraint(equalTo: topicView.leftAnchor, constant: 5).isActive = true
+        dateView.rightAnchor.constraint(equalTo: rightArrow.leftAnchor, constant: -5).isActive = true
+        dateView.bottomAnchor.constraint(lessThanOrEqualTo: self.bottomAnchor, constant: -10).isActive = true
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        //self.backgroundColor = UIColor.green
+        if let mainImage = mainImage{
+            imagePreview.image = mainImage
+        }
+        if let topic = topic{
+            topicView.text = topic
+        }
+        if let date = date{
+            dateView.text = date
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
--- MacDigger/ViewController.swift
+++ MacDigger/ViewController.swift
@@ -8,11 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tvNews: UITableView!
+    
+    var newsList = [News]()
+    
+    struct News {
+        var image : UIImage = #imageLiteral(resourceName: "empty_image")
+        let topic : String!
+        let date : String!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tvNews.delegate = self
+        tvNews.dataSource = self
+        tvNews.register(NewsCell.self, forCellReuseIdentifier: "NewsCell")
+        tvNews.rowHeight = UITableViewAutomaticDimension
+        tvNews.estimatedRowHeight = 300
+        
+        newsList = [News.init(image: #imageLiteral(resourceName: "empty_image"), topic: "Как Apple создаёт рекламые видео", date: "22 МАР, 2018"),
+        News.init(image: #imageLiteral(resourceName: "empty_image"), topic: "Разработчики игры The Walking Dead: Our World используют iPhone X для создания персонажей", date: "22 МАР, 2018"),
+        News.init(image: #imageLiteral(resourceName: "empty_image"), topic: "Apple запустит тестовое производство iPhone 2018 года в апреле", date: "22 МАР, 2018"),
+        News.init(image: #imageLiteral(resourceName: "empty_image"), topic: "Почему блокировка Telegram вызвала большой ажиотаж", date: "22 МАР, 2018"),
+        News.init(image: #imageLiteral(resourceName: "empty_image"), topic: "Apple Watch определяют аритмию с точностью 97%", date: "22 МАР, 2018")]
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +41,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return newsList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tvNews.dequeueReusableCell(withIdentifier: "NewsCell") as! NewsCell
+        cell.mainImage = newsList[indexPath.row].image
+        cell.topic = newsList[indexPath.row].topic
+        cell.date = newsList[indexPath.row].date
+        cell.layoutSubviews()
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
 }
 
